f3e25f6ced3450fa532f6ebae367480bf9d94530
jpsm-at-deec <145997651+jpsm-at-deec@users.noreply.github.com>
2024-02-22 16:45:40 +0000

    Update ros2_segway_rmp_node.cpp

--- src/ros2_segway_rmp_node.cpp
+++ src/ros2_segway_rmp_node.cpp
@@ -9,18 +9,31 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "segwayrmp/segwayrmp.h"
+#include "SegwayStatusStamped.h"
+
+
+class SegwayRMPNode;
+
+static SegwayRMPNode * segwayrmp_node_instance;
+void handleStatusWrapper(segwayrmp::SegwayStatus::Ptr ss);
+
+
 
 
 // ROS2 Node class
 class SegwayRMPNode : public rclcpp::Node{
   public:
+
     segwayrmp::SegwayRMP * segway_rmp = NULL;
     segwayrmp::InterfaceType interface_type;
     segwayrmp::SegwayRMPType segway_rmp_type;
-
+    std::string serial_port;
+    bool connected;
+    segway_rmp::SegwayStatusStamped sss_msg;
     
     /****************************************/
-    SegwayRMPNode() : Node("ros2_segway_rmp_node") {
+    SegwayRMPNode() : Node("ros2_segway_rmp_node") {         
+      std::shared_ptr<rclcpp::Node> n = rclcpp::Node::make_shared("ros2_segway_rmp_node");
       this->segway_rmp = NULL;
       this->run();
     }
@@ -43,19 +56,43 @@ class SegwayRMPNode : public rclcpp::Node{
 
     /****************************************/
     void run() {
+      this->connected = false;
       this->setupSegwayRMP();
     }
     /*--------------------------------------*/
 
     /****************************************/
     void setupSegwayRMP() {
+      std::stringstream ss;
+      ss << "Connecting to Segway RMP via ";
       this->interface_type = segwayrmp::InterfaceType::serial;
       this->segway_rmp_type = segwayrmp::SegwayRMPType::rmp200;
+      this->serial_port = "ttyUSB0";
       this->segway_rmp = new segwayrmp::SegwayRMP(this->interface_type, this->segway_rmp_type);
+      ss << "serial on serial port: " << this->serial_port;
+      this->segway_rmp->configureSerial(this->serial_port);
+
+      segwayrmp_node_instance = this;
+
+      this->segway_rmp->setStatusCallback(handleStatusWrapper);
+    }
+    /*--------------------------------------*/    
+
+    /****************************************/
+    void handleStatus(segwayrmp::SegwayStatus::Ptr &ss_ptr) {
+      if (!this->connected)
+          return;
+      
+      rclcpp::Time current_time = rclcpp::Clock(RCL_ROS_TIME).now();
+      //this->sss_msg.header.stamp = current_time;
     }
     /*--------------------------------------*/
 
 };
+
+void handleStatusWrapper(segwayrmp::SegwayStatus::Ptr ss) {
+  segwayrmp_node_instance->handleStatus(ss);
+}
 
 int main(int argc, char * argv[])
 {
