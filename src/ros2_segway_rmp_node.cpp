5a9032ebe4fb159e8d6b3bcc7547d7bfb61a2ab7
jpsm-at-deec <jpsm@deec.uc.pt>
2024-02-23 10:32:21 +0000

    handleStatus

--- src/ros2_segway_rmp_node.cpp
+++ src/ros2_segway_rmp_node.cpp
@@ -18,6 +18,8 @@
 class SegwayRMPNode;
 
 static SegwayRMPNode * segwayrmp_node_instance;
+static double degrees_to_radians = M_PI / 180.0;
+
 void handleStatusWrapper(segwayrmp::SegwayStatus::Ptr ss);
 
 
@@ -37,8 +39,9 @@ class SegwayRMPNode : public rclcpp::Node{
     double initial_integrated_left_wheel_position;
     double initial_integrated_right_wheel_position;
     double initial_integrated_turn_position;
+    double odometry_reset_duration;
     bool connected;
-    bool reset_odometry;
+    bool reset_odometry;   
     
     
     /****************************************/
@@ -116,9 +119,36 @@ class SegwayRMPNode : public rclcpp::Node{
           this->initial_integrated_right_wheel_position = ss.integrated_right_wheel_position;
           this->initial_integrated_turn_position = ss.integrated_turn_position;
           this->reset_odometry = false;
+        } else if ((current_time - this->odometry_reset_start_time).seconds() > this->odometry_reset_duration) {
+          this->initial_integrated_forward_position = ss.integrated_forward_position;
+          this->initial_integrated_left_wheel_position = ss.integrated_left_wheel_position;
+          this->initial_integrated_right_wheel_position = ss.integrated_right_wheel_position;
+          this->initial_integrated_turn_position = ss.integrated_turn_position;            
+          this->reset_odometry = false;
+        } else {
+          return; // continue waiting for odometry to be reset
         }
 
       }
+      this->sss_msg.segway.pitch_angle = ss.pitch * degrees_to_radians;
+      this->sss_msg.segway.pitch_rate = ss.pitch_rate * degrees_to_radians;
+      this->sss_msg.segway.roll_angle = ss.roll * degrees_to_radians;
+      this->sss_msg.segway.roll_rate = ss.roll_rate * degrees_to_radians;
+      this->sss_msg.segway.left_wheel_velocity = ss.left_wheel_speed;
+      this->sss_msg.segway.right_wheel_velocity = ss.right_wheel_speed;
+      this->sss_msg.segway.yaw_rate = ss.yaw_rate * degrees_to_radians;
+      this->sss_msg.segway.servo_frames = ss.servo_frames;
+      this->sss_msg.segway.left_wheel_displacement = ss.integrated_left_wheel_position - this->initial_integrated_left_wheel_position;
+      this->sss_msg.segway.right_wheel_displacement = ss.integrated_right_wheel_position - this->initial_integrated_right_wheel_position;
+      this->sss_msg.segway.forward_displacement = ss.integrated_forward_position - this->initial_integrated_forward_position;
+      this->sss_msg.segway.yaw_displacement = (ss.integrated_turn_position - this->initial_integrated_turn_position) * degrees_to_radians;
+      this->sss_msg.segway.left_motor_torque = ss.left_motor_torque;
+      this->sss_msg.segway.right_motor_torque = ss.right_motor_torque;
+      this->sss_msg.segway.operation_mode = ss.operational_mode;
+      this->sss_msg.segway.gain_schedule = ss.controller_gain_schedule;
+      this->sss_msg.segway.ui_battery = ss.ui_battery_voltage;
+      this->sss_msg.segway.powerbase_battery = ss.powerbase_battery_voltage;
+      this->sss_msg.segway.motors_enabled = (bool)(ss.motor_status);
     }
     /*--------------------------------------*/
 
