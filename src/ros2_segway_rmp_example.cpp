6494c52b0b343f73aa614048cbdc99fa0ac9c410
jpsm-at-deec <jpsm@deec.uc.pt>
2024-02-28 11:57:36 +0000

    example

--- src/ros2_segway_rmp_example.cpp
+++ src/ros2_segway_rmp_example.cpp
@@ -1,19 +1,51 @@
 #include <iostream>
+
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp/duration.hpp"
+
+#include "segway_interfaces/msg/segwaystatus.hpp"     
+#include "segway_interfaces/msg/stamped.hpp"  
+
+#include "geometry_msgs/msg/twist.hpp"
+#include "geometry_msgs/msg/twist_stamped.hpp"
+
+#include "nav_msgs/msg/odometry.hpp"
 
 // ROS2 Node class
 class SegwayRMPNode : public rclcpp::Node{
   public:
-    SegwayRMPNode() : Node("ros2_segway_rmp_node") { 
-      std::cout << "wally\n";  
+    SegwayRMPNode() : Node("ros2_segway_rmp_node") {       
+      if (this->optionaldebug): { 
+        std::cout << "SegwayRMPNode class init\n";         
+      };
+      this->setupROSComms();
+      if (this->optionaldebug): { 
+        std::cout << "SegwayRMPNode class init done\n";  
+      };
     }
     ~SegwayRMPNode() {
     }
+    void setupROSComms() {
+      if (this->optionaldebug): {
+        std::cout << "setupROSComms call\n";  
+      };
+      this->segway_status_pub = n->create_publisher<segway_interfaces::msg::Stamped>("segway_status", 1000);      
+      this->cmd_velSubscriber = n->create_subscription<geometry_msgs::msg::Twist>("cmd_vel", rclcpp::SensorDataQoS(), std::bind(&SegwayRMPNode::cmd_velCallback, this, std::placeholders::_1));
+      this->odom_pub = n->create_publisher<nav_msgs::msg::Odometry>("odom", 50);
+      if (this->optionaldebug): {
+        std::cout << "setupROSComms call done\n";  
+      };
+    }
+
+    rclcpp::Publisher<segway_interfaces::msg::Stamped>::SharedPtr segway_status_pub;    
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr cmd_velSubscriber;    
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odom_pub;  
+    bool optionaldebug = true;
 };
 
 int main(int argc, char * argv[])
 {
-  std::cout << "wally\n";  
+  std::cout << "main init\n";  
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<SegwayRMPNode>());
   rclcpp::shutdown();
