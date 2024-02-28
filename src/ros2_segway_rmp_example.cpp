584fa19a25223766e7f44905c08482174c75878d
jpsm-at-deec <jpsm@deec.uc.pt>
2024-02-28 12:26:57 +0000

    example

--- src/ros2_segway_rmp_example.cpp
+++ src/ros2_segway_rmp_example.cpp
@@ -12,7 +12,7 @@
 #include "nav_msgs/msg/odometry.hpp"
 
 
-std::shared_ptr<rclcpp::Node>  n;// = rclcpp::Node::make_shared("ros2_segway_rmp_node");
+std::shared_ptr<rclcpp::Node>  n;
 
 // ROS2 Node class
 class SegwayRMPNode : public rclcpp::Node{
@@ -27,7 +27,7 @@ class SegwayRMPNode : public rclcpp::Node{
       if (this->optionaldebug) { 
         std::cout << "SegwayRMPNode class init\n";         
       };
-      //n = rclcpp::Node::make_shared("ros2_segway_rmp_node");
+      n = rclcpp::Node::make_shared("ros2_segway_rmp_node");
       this->setupROSComms();
       if (this->optionaldebug) { 
         std::cout << "SegwayRMPNode class init done\n";  
@@ -39,7 +39,7 @@ class SegwayRMPNode : public rclcpp::Node{
       if (this->optionaldebug) {
         std::cout << "setupROSComms call\n";  
       };
-      //this->segway_status_pub = n->create_publisher<segway_interfaces::msg::Stamped>("segway_status", 1000);      
+      this->segway_status_pub = n->create_publisher<segway_interfaces::msg::Stamped>("segway_status", 1000);      
       //this->cmd_velSubscriber;// = n->create_subscription<geometry_msgs::msg::Twist>("cmd_vel", rclcpp::SensorDataQoS(), std::bind(&SegwayRMPNode::cmd_velCallback, this, std::placeholders::_1));
       //this->odom_pub = n->create_publisher<nav_msgs::msg::Odometry>("odom", 50);
       if (this->optionaldebug) {
